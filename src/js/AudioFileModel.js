c9b5ca252431528a4c3dc05c95af38c8a2ee5445
mateuszmagier <mateuszmagier@bitbucket.org>
2018-10-24 22:11:37 +0200

    Install audio-metadata; add obtaining audio file's title from metadata.

--- src/js/AudioFileModel.js
+++ src/js/AudioFileModel.js
@@ -1,20 +1,41 @@
+
+var audioMetaData = require('audio-metadata');
 /*
     audioFile: File object,
     audioController: AudioFilesController object
 */
 class AudioFileModel {
-    
+
     constructor(audioFile, audioController) {
         this.file = audioFile;
         var audiofileSplit = this.file.name.split(".");
-        this.filename = audiofileSplit.slice(0,audiofileSplit.length-1).join(".");
-        this.fileext = audiofileSplit[audiofileSplit.length-1];
+        this.filename = audiofileSplit.slice(0, audiofileSplit.length - 1).join(".");
+        this.fileext = audiofileSplit[audiofileSplit.length - 1];
         this.duration = 0;
         this.timestamp = 0;
         this.convertedTimestamp = "";
+        this.title = null;
         this.controller = audioController;
 
+
         this.waitForDuration(); // register event for audio element
+        this.obtainTitle(); // obtain title of audio file
+    }
+
+    obtainTitle() {
+        var reader = new FileReader();
+
+        reader.onload = event => {
+            var buffer = event.target.result;
+            var metadata = audioMetaData.id3v2(buffer);
+            if(metadata !== null) {
+                this.title = metadata.title;
+            }
+            else
+                console.log("Nie udało się odczytać metadanych pliku.");
+        };
+
+        reader.readAsArrayBuffer(this.file);
     }
 
     waitForDuration() {
@@ -31,26 +52,28 @@ class AudioFileModel {
             this.controller.incrementObtainedDurations();
         });
     }
-    
+
     setTimestamp(timestamp) {
         this.timestamp = timestamp;
     }
-    
+
     getTimestamp() {
         return this.timestamp;
     }
-    
+
     setConvertedTimestamp(convertedTimestamp) {
         this.convertedTimestamp = convertedTimestamp;
     }
-    
+
     getDuration() {
         return this.duration;
     }
-    
+
     printDuration() {
         console.log(`${this.timestamp} ${this.convertedTimestamp} ${this.filename}`);
     }
 }
 
-export { AudioFileModel }
+export {
+    AudioFileModel
+}
